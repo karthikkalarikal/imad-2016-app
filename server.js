d99f9ba0ac505b6867a4d65c5569f2c0454cdd4f
karthikkalarikal <karthikkalarikal009@gmail.com>
2016-09-30 10:04:29 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-1', function (req, res) {
-    res.sendFie(path.join(__dirname, 'ui', 'article-1.html'));
+app.get('/ui/article-1.html', function (req, res) {
+  res.sendFie(path.join(__dirname, 'ui', 'article-1.html'));
 });
 
 
